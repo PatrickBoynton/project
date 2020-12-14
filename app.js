c7c0cd7f6c6620181d5591ce3731d07efa7eb080
PatrickBoynton <jpboynton3@gmail.com>
2020-12-13 19:10:17 -0500

    Playing around with colors.

---
+++ app.js
@@ -0,0 +1,47 @@
+// var message = 'Hello world!';
+//
+// console.log('This is a test!');
+// console.log(message);
+//
+// var num = 1;
+// var sum  = num + num;
+//
+// console.log(sum);
+//
+// var name = 'Patrick';
+// console.log(name);
+//
+// name = 'John';
+// console.log(name);
+
+// var $sentence = "Welcome to JavaScript.";
+// var one word = "JavaScript is one word.";
+// var learning-js = "I'm excited to learn JavaScript";
+// var this = "var is a Javascript keyword, along with several other words.";
+
+// console.log($sentence);
+// console.log(one word);
+// console.log(learning-js);
+// console.log(this);
+
+// var name = 'Patrick'
+//
+// console.log(name, name, name);
+
+// var num = 2;
+// var num2 = 3;
+//
+// console.log(num * num2);
+//
+// console.log(num / num2);
+//
+// console.log(num + num2);
+
+var car = "my car broke ";
+var broken = "broke down ";
+var afterboken = "I fixed ";
+var part = "yesterday ";
+var present = "today ";
+
+
+console.log(car + broken + afterboken + part + present);
