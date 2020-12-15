f3b79b6ec5f5d413a2dcd0ca2d4f39e5a0f351c7
PatrickBoynton <jpboynton3@gmail.com>
2020-12-14 19:07:40 -0500

    Class assignments.

--- app.js
+++ app.js
@@ -37,11 +37,60 @@
 //
 // console.log(num + num2);
 
-var car = "my car broke ";
-var broken = "broke down ";
-var afterboken = "I fixed ";
-var part = "yesterday ";
-var present = "today ";
+// var car = "my car broke ";
+// var broken = "broke down ";
+// var afterboken = "I fixed ";
+// var part = "yesterday ";
+// var present = "today ";
+//
+//
+// console.log(car + broken + afterboken + part + present);
 
 
-console.log(car + broken + afterboken + part + present);
+// var veg = ['cabbage', 'carrot', 'rubarb', 'onion', 'beets'];
+//
+// var numbers = [1, 2, 3];
+//
+// var empty = [];
+//
+// console.log(veg);
+// console.log(numbers);
+// console.log(empty);
+//
+// console.log(veg.length);
+// console.log(numbers.length);
+// console.log(empty.length);
+//
+// console.log(veg[2]);
+//
+// var trees = ['pine', 'citrus', 'hemlock'];
+//
+// trees[3] = 'cherry';
+//
+// console.log(trees);
+
+// function divide(number) {
+//   return number / 2;
+// }
+//
+// var result = divide(24);
+//
+// console.log(result);
+
+
+// function sentence(string1, string2){
+//   return string1 + string2;
+// }
+//
+// var speech = sentence("Hello ", "world")
+//
+// console.log(speech);
+
+
+function difference(a, b) {
+  return a - b;
+}
+
+var result = difference(5, 8);
+
+console.log(result);
