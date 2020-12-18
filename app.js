fe63780baa84290e9c4a121476ccfcf6a5fa2a16
PatrickBoynton <jpboynton3@gmail.com>
2020-12-17 20:10:46 -0500

    Final touches of the javascript.

--- app.js
+++ app.js
@@ -87,10 +87,117 @@
 // console.log(speech);
 
 
-function difference(a, b) {
-  return a - b;
+// function difference(a, b) {
+//   return a - b;
+// }
+//
+// var result = difference(5, 8);
+//
+// console.log(result);
+
+// assignments
+
+// 1.
+// function addNumbers(numberA, numberB) {
+//     return numberA + numberB;
+//  }
+//
+//  var twoPlusTwo = addNumbers(2, 2);
+//
+//  console.log(twoPlusTwo);
+
+ // 2.
+
+//  function yell(string){
+//    return string.toUpperCase();
+//  }
+// var upper = yell('I had fun!');
+//
+// console.log(upper);
+//
+// function yell10(yell){
+//   for (var i = 0; i < 10; i++) {
+//     console.log('------');
+//     console.log(yell);
+//   }
+// }
+//
+// yell10(yell('Hello'));
+
+// 3.
+// function compareLengths(a, b){
+//   if (a.length > b.length) {
+//     console.log(a + ' Is greater.');
+//   }
+//   else {
+//     console.log(b + ' Is greater.');
+//   }
+// }
+//
+// compareLengths('Testing', 'this');
+
+// 4.
+// function isVowel(char) {
+//   char.toUpperCase();
+//
+//   if (char === 'A' || char === 'E' || char === 'I' || char === 'O'  || char === 'U') {
+//     return true;
+//   }
+//
+//   return false;
+// }
+//
+// console.log(isVowel('F'));
+
+var changeButton = document.querySelector('#change');
+var span = document.querySelector('#increase');
+var body  = document.querySelector('body');
+
+changeButton.addEventListener('click',logSpan);
+
+function logSpan() {
+  var number = parseInt(span.innerText);
+  var  button = document.querySelector('#change');
+  number += 1;
+
+  span.innerText = number;
+
+  changeButton.textContent = 'Add To Cart';
+
+  setTimeout(function(){
+    changeButton.textContent = 'Buy Now!';
+
+    console.log('Done!');
+  }, 3000);
 }
 
-var result = difference(5, 8);
+document.querySelector('#details').addEventListener('click', function() {
+  var fit = document.querySelector('.fit');
+  var reviews = document.querySelector('.reviews');
+  var details = document.querySelector('.details');
 
-console.log(result);
+  fit.style.display = 'none';
+  reviews.style.display = 'none';
+  details.style.display = 'block';
+
+});
+
+document.querySelector('#fit').addEventListener('click', function() {
+  var details = document.querySelector('.details');
+  var reviews = document.querySelector('.reviews');
+  var fit = document.querySelector('.fit');
+
+  details.style.display = 'none';
+  reviews.style.display = 'none';
+  fit.style.display = 'block';
+});
+
+document.querySelector('#rating').addEventListener('click', function() {
+  var details = document.querySelector('.details');
+  var fit = document.querySelector('.fit');
+  var reviews = document.querySelector('.reviews');
+
+  details.style.display = 'none';
+  fit.style.display = 'none';
+  reviews.style.display = 'block';
+});
